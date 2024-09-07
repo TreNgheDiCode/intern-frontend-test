73693c008821a4ac377da5c77670ef763801dc58
TreNgheDiCode <gabayan170@gmail.com>
2024-09-07 17:51:00 +0700

    Generate Cache Build

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "./out",
   output: "export",
+  generateBuildId: async () => {
+    return process.env.GIT_HASH;
+  },
 };
 
 export default nextConfig;
