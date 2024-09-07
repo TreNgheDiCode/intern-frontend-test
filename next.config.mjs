9967e523293424622c522b59ada41c8b85ee4da9
TreNgheDiCode <gabayan170@gmail.com>
2024-09-07 16:50:56 +0700

    Deploy to Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/intern-frontend-test",
+  reactStrictMode: true,
+  output: "export",
+};
 
 export default nextConfig;
